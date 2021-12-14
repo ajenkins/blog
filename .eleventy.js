f9b791124d8694cb5b16007eb4ce3eb80bf71d09
AJ Jenkins <aj.jenkins123@gmail.com>
2021-12-13 20:53:55 -0500

    Added footnotes plugin to eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const Image = require('@11ty/eleventy-img');
 const embedYouTube = require('eleventy-plugin-youtube-embed');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
+const footnotes = require('eleventy-plugin-footnotes');
 
 module.exports = function (eleventyConfig) {
   // The file types that eleventy will look at and copy into _output/
@@ -97,6 +98,9 @@ module.exports = function (eleventyConfig) {
 
   // RSS (Atom) Feed
   eleventyConfig.addPlugin(pluginRss);
+
+  // Footnotes
+  eleventyConfig.addPlugin(footnotes);
 
   return {
     // Enables the use of shortcodes within Markdown
